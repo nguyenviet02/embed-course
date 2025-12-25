0cc5c438c6e735215315dfa8a92a5ddc3bf3ba3b
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-12-25 10:32:28 +0700

    feat: add custom hook

---
+++ src/hooks/useSignatureAuth.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect } from "react";
+import { createLocalJWKSet, jwtVerify } from "jose";
+
+const jwksUrl = "https://api-beta.aicademy.org/auth/jwks";
+
+export async function validateJWTString(jwtStr) {
+  let jwks;
+  try {
+    jwks = await fetch(jwksUrl, {
+      cf: {
+        cacheTtl: 60,
+        cacheTtlByStatus: {
+          "200-299": 3600,
+          "300-599": 0,
+        },
+        cacheKey: jwksUrl,
+      },
+    });
+    if (!jwks.ok) {
+      throw new Error();
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      throw err;
+    }
+    throw new Error("Failed to fetch JWKS");
+  }
+
+  let payload;
+  try {
+    const jwksJson = await jwks.json();
+    const jwk = createLocalJWKSet(jwksJson);
+
+    const data = await jwtVerify(jwtStr, jwk);
+
+    payload = data.payload;
+    if (!payload.sub) {
+      throw new Error("Invalid user ID");
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      throw err;
+    }
+    throw new Error("Invalid JWT string");
+  }
+
+  return payload;
+}
+
+const useSignatureAuth = ({ signature }) => {
+  const [metadata, setMetadata] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(null);
+
+  const submitLesson = () => {
+    if (!metadata?.user_id) {
+      console.error("User ID not found");
+      return;
+    }
+    const message = {
+      type: "SUBMIT_LESSON",
+      payload: {
+        event: "lesson_completed",
+      },
+    };
+
+    window.postMessage(message, "*");
+  };
+
+  const submitCourse = () => {
+    if (!metadata?.user_id) {
+      console.error("User ID not found");
+      return;
+    }
+    const message = {
+      type: "SUBMIT_COURSE",
+      payload: {
+        event: "course_completed",
+      },
+    };
+
+    window.postMessage(message, "*");
+  };
+
+  useEffect(() => {
+    if (!signature) return;
+
+    const validateSignature = async () => {
+      try {
+        setLoading(true);
+        const response = await validateJWTString(signature);
+        setMetadata(response?.metadata);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    validateSignature();
+  }, [signature]);
+
+  return {
+    metadata,
+    loading,
+    error,
+    submitLesson,
+    submitCourse,
+  };
+};
+
+export default useSignatureAuth;
